9647c126ad2b9f692ed020cdc0caf7787d9e94d1
GustavoCesarSantos <gustavocs789@gmail.com>
2021-06-25 20:48:07 -0300

    Corrigido migration que cria a tabela Compliments

--- src/database/migrations/1624664397593-CreateCompliments.ts
+++ src/database/migrations/1624664397593-CreateCompliments.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateCompliments1624633623793 implements MigrationInterface {
+export class CreateCompliments1624664397593 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
