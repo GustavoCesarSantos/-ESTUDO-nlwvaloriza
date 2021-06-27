78f636e171b080d4e027244bb812212ddae51156
GustavoCesarSantos <gustavocs789@gmail.com>
2021-06-26 23:02:38 -0300

    Adicionado coluna name_custom a entidade Tag

--- src/database/entities/Tag.ts
+++ src/database/entities/Tag.ts
@@ -1,3 +1,4 @@
+import { Expose } from 'class-transformer';
 import {
   Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn,
 } from 'typeorm';
@@ -16,6 +17,11 @@ class Tag {
 
   @UpdateDateColumn()
   updated_at: Date;
+
+  @Expose({ name: 'name_custom' })
+  nameCustom(): string {
+    return `#${this.name}`;
+  }
 
   constructor() {
     if (!this.id) this.id = uuid();
